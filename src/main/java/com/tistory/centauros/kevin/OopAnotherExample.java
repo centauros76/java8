61bfebb6a3a7378ee1dd92cd45e04b538071b0ad
centauros <centauros@gmail.com>
2018-12-17 17:56:45 +0900

    OopAnotherExample create

--- src/main/java/com/tistory/centauros/kevin/OopAnotherExample.java
+++ src/main/java/com/tistory/centauros/kevin/OopAnotherExample.java
@@ -7,13 +7,31 @@ package com.tistory.centauros.kevin;
 public class OopAnotherExample {
     public static void main(String[] args) {
         final CalculatorService calculatorService = new CalculatorService();
-        System.out.println(calculatorService.calculate(1,2));
+        int additionResult = calculatorService.calculate('+', 1, 1);
+        System.out.println("result :: " + additionResult);
+        int subtractionResult = calculatorService.calculate('-', 1, 1);
+        System.out.println("result :: " + subtractionResult);
+        int multiplicationResult = calculatorService.calculate('*', 1, 1);
+        System.out.println("result :: " + multiplicationResult);
+        int divitionResult = calculatorService.calculate('/', 8, 4);
+        System.out.println("result :: " + divitionResult);
+
     }
 }
 
 class CalculatorService {
-    public int calculate(int num1, int num2) {
-        return num1 + num2;
+    public int calculate(char calculation, int num1, int num2) {
+        if (calculation == '+') {
+            return num1 + num2;
+        } else if (calculation == '-') {
+            return num1 - num2;
+        } else if (calculation == '*') {
+            return num1 * num2;
+        } else if (calculation == '/') {
+            return num1 / num2;
+        } else {
+            throw new IllegalArgumentException("Know Calculation :: " + calculation );
+        }
     }
 
 }
